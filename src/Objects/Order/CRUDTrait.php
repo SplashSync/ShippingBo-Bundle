9144d1093dd9c55929a09b3721a2e807504de365
BadPixxel <eshop.bpaquier@gmail.com>
2022-02-15 17:06:09 +0100

    WIP: ShippingBo Connector

---
+++ src/Objects/Order/CRUDTrait.php
@@ -0,0 +1,217 @@
+<?php
+
+/*
+ *  This file is part of SplashSync Project.
+ *
+ *  Copyright (C) 2015-2021 Splash Sync  <www.splashsync.com>
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ *  For the full copyright and license information, please view the LICENSE
+ *  file that was distributed with this source code.
+ */
+
+namespace Splash\Connectors\ShippingBo\Objects\Order;
+
+use Exception;
+use Splash\Connectors\ShippingBo\Models\Api\Order;
+use Splash\Core\SplashCore as Splash;
+use Splash\OpenApi\Models\Objects\CRUDTrait as OpenApiCRUDTrait;
+
+/**
+ * ShippingBo Orders CRUD Functions
+ */
+trait CRUDTrait
+{
+    use OpenApiCRUDTrait{
+        OpenApiCRUDTrait::load as coreLoad;
+        OpenApiCRUDTrait::create as coreCreate;
+        OpenApiCRUDTrait::update as coreUpdate;
+    }
+
+    /**
+     * Load Request Object
+     *
+     * @param string $objectId Object id
+     *
+     * @return false|Order
+     */
+    public function load($objectId)
+    {
+        //====================================================================//
+        // Stack Trace
+        Splash::log()->trace();
+        //====================================================================//
+        // Detect Rejected Order Id => Init Rejected Object
+        if ($this->isRejectedId($objectId)) {
+            return $this->initRejected();
+        }
+        //====================================================================//
+        // Execute Core Action
+        return $this->coreLoad($objectId);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function create()
+    {
+        //====================================================================//
+        // Stack Trace
+        Splash::log()->trace();
+        //====================================================================//
+        // Ensure Default Source
+        $this->in['source'] = $this->in['source'] ?? "SplashSync";
+        //====================================================================//
+        // Ensure Default State
+        $this->in['state'] = $this->in['state'] ?? "rejected";
+        //====================================================================//
+        // Check if Order is Allowed for Creation
+        if (!$this->isAllowedDate()) {
+            // Filtered By Date
+            $this->logFilteredDate();
+        } elseif (!$this->isAllowedDeliveryService()) {
+            // Filtered By Delivery Service Name
+            $this->logFilteredMethod();
+        } elseif(!$this->isAllowedOrigin()) {
+            // Filtered By Origin
+            $this->logFilteredOrigin();
+        } else {
+            //====================================================================//
+            // Execute Core Action
+            return $this->coreCreate();
+        }
+
+        return $this->initRejected();
+    }
+
+
+    /**
+     * {@inheritDoc}
+     *
+     * @throws Exception
+     *
+     * @return false|string Object ID of False if Failed to Update
+     */
+    public function update(bool $needed)
+    {
+        //====================================================================//
+        // Detect Rejected Order Id => Init Rejected Object
+        if ($this->isRejectedId($this->object->id)) {
+            return $this->getObjectIdentifier();
+        }
+        //====================================================================//
+        // Execute Core Action
+        $response = $this->coreUpdate($needed);
+        if (false == $response) {
+            return false;
+        }
+        //====================================================================//
+        // Update Order Shipping Address
+        if ($this->isToUpdate("ShippingAddress") && !$this->updateShippingAddress()) {
+            return false;
+        }
+        //====================================================================//
+        // Update Order Items
+        if ($this->isToUpdate("Items")) {
+            if (!$this->updateOrderItems()) {
+                return false;
+            }
+            if (!$this->computeOrderItems()) {
+                return false;
+            }
+        }
+
+        return $response;
+    }
+
+    /**
+     * Update Order Shipping Address after Main Update
+     *
+     * @throws Exception
+     *
+     * @return bool
+     */
+    protected function updateShippingAddress(): bool
+    {
+        $address = $this->object->shippingAddress;
+        $addressId = $this->object->getShippingAddressId() ?? $address->id;
+        //====================================================================//
+        // Safety Check
+        if (!$address || empty($addressId)) {
+            return false;
+        }
+        //====================================================================//
+        // Build Request Uri
+        $uri = "/addresses/".$addressId;
+        $body = $this->getVisitor()->getHydrator()->extract($address);
+        //====================================================================//
+        // Execute Request
+        if (!$this->getVisitor()->getConnexion()->patch($uri, $body)) {
+            return Splash::log()->err("An error occurred while updating Order Shipping Address");
+        }
+        Splash::log()->war("Order Shipping Address Updated");
+
+        return true;
+    }
+
+    /**
+     * Update Order Items after Main Update
+     *
+     * @throws Exception
+     *
+     * @return bool
+     */
+    private function updateOrderItems(): bool
+    {
+        //====================================================================//
+        // Serialize All Order Items
+        $orderItems = array();
+        foreach ($this->object->items as $index => $item) {
+            $item->source = $this->object->source;
+            $item->source_ref = $this->object->source_ref."-".($index + 1);
+            $orderItems[$index] = $this->getVisitor()->getHydrator()->extract($item);
+            unset($orderItems[$index]['price']);
+        }
+        //====================================================================//
+        // Build Request Uri
+        $uri = $this->visitor->getItemUri((string) $this->getObjectIdentifier());
+        $uri .= "/update_order_items";
+        $body = array(
+            "order_items" => $orderItems
+        );
+        //====================================================================//
+        // Execute Request
+        if (!$this->getVisitor()->getConnexion()->post($uri, $body)) {
+            return Splash::log()->err("An error occurred while updating Order Items");
+        }
+        Splash::log()->war("Order Items Updated");
+
+        return true;
+    }
+
+    /**
+     * Update Order Items after Main Update
+     *
+     * @throws Exception
+     *
+     * @return bool
+     */
+    private function computeOrderItems(): bool
+    {
+        //====================================================================//
+        // Build Request Uri
+        $uri = $this->visitor->getItemUri((string) $this->getObjectIdentifier());
+        $uri .= "/recompute_mapped_products";
+        //====================================================================//
+        // Execute Request
+        if (!$this->getVisitor()->getConnexion()->post($uri, array())) {
+            return Splash::log()->err("An error occurred while computing Order Items");
+        }
+        Splash::log()->war("Order Items Computed");
+
+        return true;
+    }
+}
---
+++ src/Services/ShippingBoConnector.php
@@ -0,0 +1,441 @@
+<?php
+
+/*
+ *  This file is part of SplashSync Project.
+ *
+ *  Copyright (C) 2015-2021 Splash Sync  <www.splashsync.com>
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ *  For the full copyright and license information, please view the LICENSE
+ *  file that was distributed with this source code.
+ */
+
+namespace Splash\Connectors\ShippingBo\Services;
+
+use ArrayObject;
+use Exception;
+use Httpful\Mime;
+use Splash\Bundle\Interfaces\Connectors\TrackingInterface;
+use Splash\Bundle\Models\AbstractConnector;
+use Splash\Bundle\Models\Connectors\GenericObjectMapperTrait;
+use Splash\Bundle\Models\Connectors\GenericWidgetMapperTrait;
+use Splash\Connectors\ShippingBo\Form\DebugFormType;
+use Splash\Connectors\ShippingBo\Form\EditFormType;
+use Splash\Connectors\ShippingBo\Hydrator\Hydrator;
+use Splash\Connectors\ShippingBo\Objects;
+use Splash\Connectors\ShippingBo\Widgets;
+use Splash\Core\SplashCore as Splash;
+use Splash\OpenApi\Action;
+use Splash\OpenApi\Connexion\JsonConnexion;
+use Splash\OpenApi\Models\Connexion\ConnexionInterface;
+
+/**
+ * ShippingBo REST API Connector for Splash
+ *
+ * @SuppressWarnings(PHPMD.ExcessiveClassComplexity)
+ */
+class ShippingBoConnector extends AbstractConnector implements TrackingInterface
+{
+    use GenericObjectMapperTrait;
+    use GenericWidgetMapperTrait;
+
+    /**
+     * Objects Type Class Map
+     *
+     * @var array<string, class-string>
+     */
+    protected static array $objectsMap = array(
+        "Order" => Objects\Order::class,
+        "Product" => Objects\Product::class,
+        "Address" => Objects\Address::class,
+    );
+
+    /**
+     * Widgets Type Class Map
+     *
+     * @var array
+     */
+    protected static array $widgetsMap = array(
+        "SelfTest" => Widgets\SelfTest::class,
+    );
+
+    /**
+     * @var ConnexionInterface
+     */
+    private ConnexionInterface $connexion;
+
+    /**
+     * Object Hydrator
+     *
+     * @var Hydrator
+     */
+    private Hydrator $hydrator;
+
+    /**
+     * @var string
+     */
+    private string $metaDir;
+
+    /**
+     * Setup Cache Dir for Metadata
+     */
+    public function setMetaDir(string $metaDir) : void
+    {
+        $this->metaDir = $metaDir."/metadata/shippingbo";
+    }
+
+    /**
+     * {@inheritdoc}
+     * @throws Exception
+     */
+    public function ping() : bool
+    {
+        //====================================================================//
+        // Safety Check => Verify Self-test Pass
+        if (!$this->selfTest()) {
+            return false;
+        }
+
+        //====================================================================//
+        // Perform Ping Test
+        return Action\Ping::execute($this->getConnexion(), "/products");
+    }
+
+    /**
+     * {@inheritdoc}
+     * @throws Exception
+     */
+    public function connect() : bool
+    {
+        //====================================================================//
+        // Safety Check => Verify Self-test Pass
+        if (!$this->selfTest()) {
+            return false;
+        }
+        //====================================================================//
+        // Get User Information
+        if (!$this->fetchUserInformation()) {
+            return false;
+        }
+        //====================================================================//
+        // Get Available Shipping Methods
+        if (!$this->fetchLogisticServices()) {
+            return false;
+        }
+        //====================================================================//
+        // Update Connector Settings
+        $this->updateConfiguration();
+
+        return true;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function informations(ArrayObject  $informations) : ArrayObject
+    {
+        //====================================================================//
+        // Server General Description
+        $informations->shortdesc = "ShippingBo API";
+        $informations->longdesc = "Splash Integration for ShippingBo OpenApi V1";
+        //====================================================================//
+        // Company Informations
+        $informations->company = "ShippingBo";
+        $informations->address = "3 Av. de l'Europe Bat D";
+        $informations->zip = "31400";
+        $informations->town = "Toulouse";
+        $informations->country = "France";
+        $informations->www = "https://shippingbo.com/";
+        $informations->email = "contact@shippingbo.com";
+        $informations->phone = "+33 (0) 5 34 41 23 43";
+        //====================================================================//
+        // Server Logo & Ico
+        $informations->icoraw = Splash::file()->readFileContents(
+            dirname(__FILE__, 2) ."/Resources/public/img/ShippingBo-Icon.jpg"
+        );
+        $informations->logourl = null;
+        $informations->logoraw = Splash::file()->readFileContents(
+            dirname(__FILE__, 2) ."/Resources/public/img/ShippingBo-Logo.jpg"
+        );
+        //====================================================================//
+        // Server Informations
+        $informations->servertype = "ShippingBo Api V1";
+        $informations->serverurl = "shippingbo.com";
+        //====================================================================//
+        // Module Informations
+        $informations->moduleauthor = "Splash Official <www.splashsync.com>";
+        $informations->moduleversion = "master";
+
+        return $informations;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function selfTest() : bool
+    {
+        $config = $this->getConfiguration();
+
+        //====================================================================//
+        // Verify Api User is Set
+        //====================================================================//
+        if (empty($config["ApiUser"]) || !is_string($config["ApiUser"])) {
+            Splash::log()->err("Api User is Invalid");
+
+            return false;
+        }
+
+        //====================================================================//
+        // Verify Api Key is Set
+        //====================================================================//
+        if (empty($config["ApiKey"]) || !is_string($config["ApiKey"])) {
+            Splash::log()->err("Api Key is Invalid");
+
+            return false;
+        }
+
+        return true;
+    }
+
+    //====================================================================//
+    // Files Interfaces
+    //====================================================================//
+
+    /**
+     * {@inheritdoc}
+     *
+     * @throws Exception
+     */
+    public function getFile(string $filePath, string $fileMd5)
+    {
+        //====================================================================//
+        // Safety Check => Verify Self-test Pass
+        if (!$this->selfTest()) {
+            return false;
+        }
+        //====================================================================//
+        // Read File Contents via Raw Get Request
+        $rawResponse = $this->getConnexion()->getRaw($filePath);
+        if (!$rawResponse || (md5($rawResponse) != $fileMd5)) {
+            return false;
+        }
+        //====================================================================//
+        // Build File Array
+        $file = array();
+        $file["name"] = $file["filename"] = pathinfo($filePath, PATHINFO_BASENAME);
+        $file["path"] = $filePath;
+        $file["url"] = $filePath;
+        $file["raw"] = base64_encode((string) $rawResponse);
+        $file["md5"] = md5($rawResponse);
+        $file["size"] = strlen($rawResponse);
+
+        return $file;
+    }
+
+    //====================================================================//
+    // Profile Interfaces
+    //====================================================================//
+
+    /**
+     * Get Connector Profile Information
+     *
+     * @return array
+     */
+    public function getProfile() : array
+    {
+        return array(
+            'enabled' => true,                                      // is Connector Enabled
+            'beta' => false,                                        // is this a Beta release
+            'type' => self::TYPE_HIDDEN,                            // Connector Type or Mode
+            'name' => 'shippingbo',                                 // Connector code (lowercase, no space allowed)
+            'connector' => 'splash.connectors.shippingbo',          // Connector Symfony Service
+            'title' => 'profile.card.title',                        // Public short name
+            'label' => 'profile.card.label',                        // Public long name
+            'domain' => 'ShippingBoBundle',                         // Translation domain for names
+            'ico' => '/bundles/shippingbo/img/ShippingBo-Icon.jpg', // Public Icon path
+            'www' => 'https://shippingbo.com',                      // Website Url
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getConnectedTemplate() : string
+    {
+        return "@ShippingBo/Profile/connected.html.twig";
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getOfflineTemplate() : string
+    {
+        return "@ShippingBo/Profile/offline.html.twig";
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getNewTemplate() : string
+    {
+        return "@ShippingBo/Profile/new.html.twig";
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getFormBuilderName() : string
+    {
+        $this->selfTest();
+
+        if ($this->getParameter("isSandbox", false)) {
+            return DebugFormType::class;
+        }
+
+        return EditFormType::class;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getMasterAction(): ?string
+    {
+        return null;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getPublicActions() : array
+    {
+        return array(
+            "webhook" => "ShippingBoBundle:WebHooks:index",
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getSecuredActions() : array
+    {
+        return array();
+    }
+
+    //====================================================================//
+    // ReCommerce Connector Specific
+    //====================================================================//
+
+    /**
+     * Check if Connector use Sandbox Mode
+     *
+     * @return bool
+     */
+    public function isSandbox(): bool
+    {
+        if ($this->getParameter("isSandbox", false)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    //====================================================================//
+    // Open API Connector Interfaces
+    //====================================================================//
+
+    /**
+     * Get Connector Api Connexion
+     *
+     * @throws Exception
+     *
+     * @return ConnexionInterface
+     */
+    public function getConnexion() : ConnexionInterface
+    {
+        //====================================================================//
+        // Connexion already created
+        if (isset($this->connexion)) {
+            return $this->connexion;
+        }
+        //====================================================================//
+        // Safety check
+        if (!$this->selfTest()) {
+            throw new Exception("Self-test fails... Unable to create API Connexion!");
+        }
+        $config = $this->getConfiguration();
+        //====================================================================//
+        // Detect Api Url
+        $url = $this->getParameter("isSandbox", false)
+            ? $config["WsHost"]
+            : "https://app.shippingbo.com"
+        ;
+        //====================================================================//
+        // Setup Api Connexion
+        $this->connexion = new JsonConnexion($url, array(
+            'X-API-USER' => $config["ApiUser"],
+            'X-API-TOKEN' => $config["ApiKey"],
+            'X-API-VERSION' => 1,
+        ));
+        if (!$this->isSandbox()) {
+            $this->connexion->setPatchMimeType(Mime::JSON);
+        }
+
+        return $this->connexion;
+    }
+
+    /**
+     * @return Hydrator
+     */
+    public function getHydrator(): Hydrator
+    {
+        //====================================================================//
+        // Configure Object Hydrator
+        if (!isset($this->hydrator)) {
+            $this->hydrator = new Hydrator($this->metaDir);
+        }
+
+        return $this->hydrator;
+    }
+
+    /**
+     * Get ShippingBo User Information
+     *
+     * @return bool
+     * @throws Exception
+     */
+    private function fetchUserInformation(): bool
+    {
+        //====================================================================//
+        // Get User Infos from Api
+        $response = $this->getConnexion()->get("/users/me");
+        if (!isset($response["user"]) || !is_array($response["user"])) {
+            return false;
+        }
+        //====================================================================//
+        // Store in Connector Settings
+        $this->setParameter("UserInformations", $response["user"]);
+
+        return true;
+    }
+
+    /**
+     * Get ShippingBo Logistic Services Configuration
+     *
+     * @return bool
+     */
+    private function fetchLogisticServices(): bool
+    {
+        //====================================================================//
+        // Store in Connector Settings
+        $this->setParameter(
+            "ShippingMethodChoices",
+            EditFormType::getStaticShippingMethodsChoices()
+        );
+
+        return true;
+    }
+
+}
